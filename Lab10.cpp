9959ed38e175d0ebfe5e00c5dbb0fb252430ac3e
MojoNojo06 <azaleah.j06@gmail.com>
2025-11-05 21:46:38 -0600

    file reading

--- Lab10.cpp
+++ Lab10.cpp
@@ -2,6 +2,7 @@
 #include <regex>
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -62,7 +63,7 @@ bool checkValid(string input) {
 }
 
 string numberToString(Number input) {
-    return input.integer + '.' + input.decimal;
+    return input.integer;
 }
 
 Number toNumber(string input) {
@@ -94,8 +95,45 @@ Number toNumber(string input) {
     return returnNumber;
 }
 
-string addStrings(string num1, string num2, bool neg) {
+string addStrings(string num1, string num2) {
+    bool num1Negative = false;
+    bool num2Negative = false;
     string sum;
+    if (checkValid(num1)) {
+        if (num1[0] == '-') {
+            num1Negative = true;
+            num1 = num1.substr(1);
+        } else if (num1[0] == '+') {
+            num1Negative = false;
+            num1 = num1.substr(1);
+        } else {
+            num1Negative = false;
+        }
+    } else {
+        return "["+ num1 + "] is an invalid input!";
+    }
+
+    if (checkValid(num2)) {
+        if (num2[0] == '-') {
+            num2Negative = true;
+            num2 = num2.substr(1);
+        } else if (num2[0] == '+') {
+            num2Negative = false;
+            num2 = num1.substr(1);
+        } else {
+            num2Negative = false;
+        }
+    } else {
+        return "["+ num2 + "] is an invalid input!";
+    }
+
+    if (num1Negative && !num2Negative) {
+        string temp = num1;
+        num1 = num2;
+        num2 = temp;
+        num2Negative = true;
+        num1Negative = false;
+    }
 
     if (num1.length() < num2.length()) {
         num1.insert(num1.begin(), num2.length() - num1.length(), '0');
@@ -112,12 +150,12 @@ string addStrings(string num1, string num2, bool neg) {
             sum.insert(sum.begin(), '.');
             continue;
         }
-        if (neg) {
+        if (num2Negative) {
             asciiSum = num1[i] - num2[i] + charOffset - carry;
             carry = false;
 
             if (asciiSum - charOffset < 0) {
-                if (i == 0 && neg) {
+                if (i == 0 && num2Negative) {
                     goesNegative = true;
                 }
                 cout << "carrying, jumped from " << asciiSum - charOffset;
@@ -148,45 +186,34 @@ string addStrings(string num1, string num2, bool neg) {
             oneDigitMore += '0';
         }
         cout << "negativizing by performing " << oneDigitMore << " - " << sum << endl;
-        sum = addStrings(oneDigitMore, sum, true);
+        sum = addStrings(oneDigitMore, '-' + sum);
         sum.insert(sum.begin(), '-');
     } else if (carry && !goesNegative) {
         sum.insert(sum.begin(), '1');
     }
 
+    if (num1Negative && num2Negative) {
+        sum += '-';
+    }
+
     return sum;
 }
 
-Number add(Number num1, Number num2) {
-    Number addedNumbers;
-
-    string added;
-    if (num1.negative && num2.negative) {
-        added = addStrings(num1.integer + '.' + num1.decimal, num2.integer + '.' + num2.decimal, false);
-    } else if (num1.negative) {
-        added = addStrings(num2.integer + '.' + num2.decimal, num1.integer + '.' + num1.decimal, true);
-    } else if (num2.negative) {
-        added = addStrings(num1.integer + '.' + num1.decimal, num2.integer + '.' + num2.decimal, true);
-    } else {
-        added = addStrings(num1.integer + '.' + num1.decimal, num2.integer + '.' + num2.decimal, false);
-    }
-
-    addedNumbers.integer = added;
-
-    return addedNumbers;
-}
-
 int main() {
-    string number = "-905.7 -903.8";
+    string line;
+    ifstream inputFile("inputfile.txt");
 
-    vector<string> splitNumber = splitString(number, ' ');
-
-    Number num1 = toNumber(splitNumber[0]);
-    cout << "num1: " << numberToString(num1) << endl;
-    Number num2 = toNumber(splitNumber[1]);
-    cout << "num2: " << numberToString(num2) << endl;
-
-    cout << "added: " << numberToString(add(num1, num2)) << endl;
+    if (inputFile.is_open()) {
+        while(getline(inputFile, line)) {
+            vector<string> splitNumbers = splitString(line, ' ');
+            cout << "num1: " << splitNumbers[0] << endl;
+            cout << "num2: " << splitNumbers[1] << endl;
+            cout << "added: " << addStrings(splitNumbers[0], splitNumbers[1]) << endl;
+            cout << "=========" << endl;
+        }
+    } else {
+        cout << "Could not open the input file!\n";
+    }
     
     return 0;
 }
